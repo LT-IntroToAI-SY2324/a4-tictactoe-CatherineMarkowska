33ff95e4a7ac486d910232b8dfa08b61294cb28d
Catherine Markowska <cimarkowska@cps.edu>
2023-10-25 10:59:07 -0500

    finished writeup and worked on a4

--- a4.py
+++ a4.py
@@ -2,6 +2,9 @@
 # talking about unimplemented class attributes, don't worry about this as you're working
 
 
+from telnetlib import WONT
+
+
 class TTTBoard:
     """A tic tac toe board
 
@@ -10,8 +13,41 @@ class TTTBoard:
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
 
-    pass
+    def __init__(self):
+        self.board = ["*"] * 9
 
+    def __str__(self) -> str:
+        s = ""
+        for x in [0, 3, 6]:
+            s += self.board[x+0] + " " + self.board[x+1] + " " + self.board[x+2] + "\n"
+        return s
+
+    def make_move(self, player, pos) -> bool:
+        # fail if invalid move 
+        if pos < 0 or pos > 8 or self.board[pos] != "*":
+            return False
+
+        # otherwise make the move 
+        self.board[pos] = player
+        return True
+
+    def has_won(self, player) -> bool:
+        if player == "XXX" or player == "OOO":
+            return True 
+        else:
+            return False
+    
+    def game_over(self) -> bool:
+        if self.board == :
+            return True
+        else 
+            return False
+    
+    def clear(self) -> str:
+        s = ""
+        for x in [0, 3, 6]:
+            s += self.board[x+0] + " " + self.board[x+1] + " " + self.board[x+2] + "\n"
+        return s
 
 def play_tic_tac_toe() -> None:
     """Uses your class to play TicTacToe"""
@@ -61,9 +97,12 @@ if __name__ == "__main__":
     # need to write some more tests to make sure that your TTTBoard class is behaving
     # properly.
     brd = TTTBoard()
+    print(brd.board)
+    print(brd)
+    
     brd.make_move("X", 8)
     brd.make_move("O", 7)
-
+    print(brd)
     assert brd.game_over() == False
 
     brd.make_move("X", 5)
